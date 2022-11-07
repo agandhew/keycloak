6f94db5348e1cf4687c36ac039948e416c81111a
Alice Wood <alwood@redhat.com>
2022-11-07 08:33:58 -0600

    Fix minor formatting and boolean logic errors

--- testsuite/utils/src/main/java/org/keycloak/testsuite/util/cli/IdentityProviderCommands.java
+++ testsuite/utils/src/main/java/org/keycloak/testsuite/util/cli/IdentityProviderCommands.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class IdentityProviderCommand {
+public class IdentityProviderCommands {
 
     public static class Create extends AbstractCommand {
 
